d077a23e5031262e34b21bb489afb6bffe9bc796
Y-20180219WJVFC\Administrator <skjmy@Outlook.com>
2018-03-31 16:52:25 +0800

    Add Project File

---
+++ src/app/hero.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable} from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class HeroService {
+
+  private heroesUrl = 'api/heroes';
+  
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  // getHero(id: number): Observable<Hero> {
+  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id === id));
+  // }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+}
